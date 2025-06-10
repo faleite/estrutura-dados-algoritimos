c2d45e88fb886db690024fbc26edd8c1bc61cbac
faleite <fabricio_2310@hotmail.com>
2025-06-10 22:22:51 +0100

    Add `remove` method to Vetor class and create new test classes for object handling
    Implemented the `remove` method in `Vetor` for element removal by position. Created new classes (`VetorObjetos`, `Contato`, `Aula09`, and `Aula10`) to handle objects in dynamic arrays, showcase features like object equality, and validate operations including addition, search, and removal.

--- src/com/faleite/estruturadados/vetor/VetorObjetos.java
+++ src/com/faleite/estruturadados/vetor/VetorObjetos.java
@@ -1,40 +1,15 @@
 package com.faleite.estruturadados.vetor;
 
-public class Vetor {
-    private String[] elementos;
+public class VetorObjetos {
+    private Object[] elementos;
     private int tamanho;
 
-    public Vetor(int capacidade){
-        this.elementos = new String[capacidade];
+    public VetorObjetos(int capacidade){
+        this.elementos = new Object[capacidade];
         this.tamanho = 0;
     }
 
-    /*public void adiciona(String elemento){
-
-        this.aumentaCapacidade();
-
-        for (int i = 0; i < elementos.length; i++) {
-            if (elementos[i] == null) {
-                elementos[i] = elemento;
-                break;
-            }
-        }
-    }*/
-
-    /*public void adiciona(String elemento) throws Exception {
-
-        this.aumentaCapacidade();
-
-        if (this.tamanho < this.elementos.length) {
-            this.elementos[this.tamanho] = elemento;
-            this.tamanho++;
-        } else {
-            throw new Exception("Vetor já está cheio, não é possivel adionar mais elementos");
-        }
-
-    }*/
-
-    public boolean adiciona(String elemento) {
+    public boolean adiciona(Object elemento) {
 
         this.aumentaCapacidade();
 
@@ -47,7 +22,7 @@ public class Vetor {
         return false;
     }
 
-    public boolean adiciona(int posicao, String elemento) {
+    public boolean adiciona(int posicao, Object elemento) {
 
         if (!(posicao >= 0 && posicao < this.tamanho)){
             throw new IllegalArgumentException("Posição inválida");
@@ -82,7 +57,7 @@ public class Vetor {
     private void aumentaCapacidade() {
 
         if (this.tamanho == this.elementos.length) {
-            String[] elementosNovos = new String[this.elementos.length * 2];
+            Object[] elementosNovos = new Object[this.elementos.length * 2];
             for (int i = 0; i < this.elementos.length; i++){
                 elementosNovos[i] = this.elementos[i];
             }
@@ -91,7 +66,7 @@ public class Vetor {
 
     }
 
-    public String busca(int posicao){
+    public Object busca(int posicao){
 
         if (!(posicao >= 0 && posicao < this.tamanho)){
             throw new IllegalArgumentException("Posição inválida");
@@ -100,7 +75,7 @@ public class Vetor {
         return this.elementos[posicao];
     }
 
-    public int busca(String elemento){
+    public int busca(Object elemento){
 
         for (int i = 0; i < this.tamanho; i++) {
             if (this.elementos[i].equals(elemento)){
