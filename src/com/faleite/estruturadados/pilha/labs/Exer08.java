9b7fe7348e014ca7e2fad2eba4107bce1bcdb1d3
faleite <fabricio_2310@hotmail.com>
2025-07-02 22:31:34 +0100

    Add `Exer08` class to implement Tower of Hanoi solution using Java `Stack`

---
+++ src/com/faleite/estruturadados/pilha/labs/Exer08.java
@@ -0,0 +1,36 @@
+package com.faleite.estruturadados.pilha.labs;
+
+import java.util.Stack;
+
+public class Exer08 {
+
+    public static void torreHanoi(int numDiscos, Stack<Integer> original, Stack<Integer> dest, Stack<Integer> aux){
+
+        if (numDiscos > 0){
+            torreHanoi(numDiscos-1, original, aux, dest);
+            dest.push(original.pop());
+            System.out.println("-------");
+            System.out.println("Original: " + original);
+            System.out.println("Destino: " + dest);
+            System.out.println("Auxiliar: " + aux);
+            torreHanoi(numDiscos-1, aux, dest, original);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> original = new Stack<>();
+        Stack<Integer> dest = new Stack<>();
+        Stack<Integer> aux = new Stack<>();
+        int numDiscos = 5;
+
+        for (int i = numDiscos; i > 0; i--){
+            original.push(i);
+        }
+
+        torreHanoi(numDiscos, original, aux, dest);
+    }
+}
+
+/*
+
+* */
